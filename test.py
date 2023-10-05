966d3f59c574641b854dbfc0e755126830bcc0a0
Guillermo Izquierdo <6guishe@gmail.com>
2023-10-05 14:40:42 +0200

    commit test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import get_data
 
 team_ids_dict=get_data.create_team_ids_dict()
 
-# team_stats_df=get_data.get_team_stats(team_ids_dict)
+team_stats_df=get_data.get_team_stats(team_ids_dict)
 
-# print(team_stats_df)
+print(team_stats_df)
 
 team_hist_df=get_data.get_historical_data(team_ids_dict)
 
